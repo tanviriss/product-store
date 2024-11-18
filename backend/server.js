be19399935afe3a70186d95d1bd4d9bd61133c71
Tanvir <Tanvirislamnyc@gmail.com>
2024-11-18 17:34:27 -0500

    finished backend

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // allows us to accept json data in the req.body
 
-app.post("/api/products", async (req, res) => {
-  const product = req.body; // user will send this data
+app.use("/api/products", productRoutes);
 
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" });
-  }
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("Error in create product:", error.message);
-    res.status(500).json({ success: false, nessage: "Server Error" });
-  }
-});
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:3000");
+  console.log("Server started at http://localhost:" + PORT);
 });
