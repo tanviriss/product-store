bc65c0a1337ae2dc87938597b77cd733804b1a0f
Tanvir <Tanvirislamnyc@gmail.com>
2024-11-18 19:18:46 -0500

    imported chakra ui + started frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css'; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App/>
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
